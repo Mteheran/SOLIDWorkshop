d03bfed90471d2e0572fa4bdd4aae6ab81be11dd
Miguel Teheran <mteherangarcia@trueblue.com>
2023-10-22 15:53:46 -0500

    aading demo 4

---
+++ Single Resposibility Principle/Demo_4/Program.cs
@@ -0,0 +1,58 @@
+﻿
+var myGame = new Game();
+myGame.Run();
+
+
+public class Game
+{
+    private bool isRunning;
+    private int playerX;
+    private int playerY;
+
+    public Game()
+    {
+        isRunning = true;
+        playerX = 0;
+        playerY = 0;
+    }
+
+    public void Run()
+    {
+        while (isRunning)
+        {
+            // Game logic
+            ConsoleKeyInfo keyInfo = Console.ReadKey();
+            HandleInput(keyInfo);
+            Update();
+            Render();
+        }
+    }
+
+    private void HandleInput(ConsoleKeyInfo keyInfo)
+    {
+        // Handle user input to move the player
+        if (keyInfo.Key == ConsoleKey.LeftArrow)
+            playerX--;
+        else if (keyInfo.Key == ConsoleKey.RightArrow)
+            playerX++;
+        else if (keyInfo.Key == ConsoleKey.UpArrow)
+            playerY--;
+        else if (keyInfo.Key == ConsoleKey.DownArrow)
+            playerY++;
+        else if (keyInfo.Key == ConsoleKey.Escape)
+            isRunning = false;
+    }
+
+    private void Update()
+    {
+        // Update game state
+    }
+
+    private void Render()
+    {
+        // Render the game, including the player's position
+        Console.Clear();
+        Console.SetCursorPosition(playerX, playerY);
+        Console.Write("*");
+    }
+}
